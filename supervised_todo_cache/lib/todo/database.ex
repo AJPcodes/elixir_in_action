96361de62bcf887e3db4b5e5400126426698ab02
Allen Phelps <aphelps@aspirehealthcare.com>
2017-07-22 18:34:59 -0500

    chapter 8

---
+++ supervised_todo_cache/lib/todo/database.ex
@@ -0,0 +1,44 @@
+defmodule Todo.Database do
+  use GenServer
+
+  def start_link(db_folder) do
+    GenServer.start_link(__MODULE__, db_folder, name: :database_server)
+  end
+
+  def store(key, data) do 
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.store(key, data)
+  end
+
+  def get(key) do
+    key
+    |> choose_worker
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  defp choose_worker(key) do
+    GenServer.call(:database_server, {:choose_worker, key})
+  end
+
+  def init(db_folder) do
+    workers = start_workers(db_folder)
+    IO.inspect(workers)
+    {:ok, workers}
+  end
+
+  defp start_workers(db_folder) do
+    for index <- 1..3, into: Map.new do
+      {:ok, pid} = Todo.DatabaseWorker.start_link(db_folder)
+      {index - 1, pid}
+    end
+  end
+
+  def handle_call({:choose_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_key), workers}
+  end
+
+end
+
+# Todo.Server.add_entry(bobs_list, %{date: {2013, 12, 19}, title: "Dentist"})
